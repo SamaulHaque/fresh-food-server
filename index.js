fa3b1b56c0a121128e7d15db1305434d4e53bcda
SamaulHaque <samaulhaque749@gmail.com>
2022-11-16 16:03:02 +0600

    create api to load my services data from mongodb

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middle wires
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pavt7kq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('freshFood').collection('services');
+
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.error(error))
+
 
 app.get('/', (req, res) => {
     res.send('food server is running')
